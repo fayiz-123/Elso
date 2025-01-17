83b1e40a8fcf93902aeb1526376eda3b8d0c8da6
Fayiz <fayizmohd211@gmail.com>
2025-01-17 14:14:39 +0530

    made first change

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+
+        helloo i am just learing git...
       </p>
     </>
   )
